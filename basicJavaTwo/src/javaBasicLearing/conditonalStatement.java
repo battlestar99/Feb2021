4daf20837ef375427d6312ca854efc7e003cd9a9
Anik <anik@agiletech.com>
2021-02-25 14:40:57 -0500

    Help session 1

---
+++ basicJavaTwo/src/javaBasicLearing/conditonalStatement.java
@@ -0,0 +1,60 @@
+package javaBasicLearing;
+
+public class conditonalStatement {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// how to declare a boolean variable 
+		
+		// declaretion and initialzation in the same lin
+		boolean x = true; 
+		
+		// declaretion and initiazltion in different line
+		boolean y; 
+		
+		y = false; 
+		
+	// there are two numbers and we are going to compare them 
+		
+		int number1 ; 
+		int number2 ; 
+		
+		number1 = 500; 
+		number2= 500; 
+		
+		
+		x= (number1==number2);
+		y=(number1>number2);
+		
+		
+		
+		if(x) {
+			
+			System.out.println("Numbers are equal");
+		}
+		
+		if(y) {
+			
+			System.out.println("Number1 is greater than number2");
+			
+		}
+		
+		if(number1<number2) {
+			
+			System.out.println("Number1 is less than number2");
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
