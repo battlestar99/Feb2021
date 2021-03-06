a991e51657fec47a761a2637689cdd71d99da27c
Anik <anik@agiletech.com>
2021-03-06 11:50:21 -0500

    first selenium script

---
+++ seleniumTest/src/seleniumTestPack/firstScript.java
@@ -0,0 +1,28 @@
+package seleniumTestPack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class firstScript {
+
+	public static void main(String[] args) {
+		
+	// setting up the properties for browser  
+		
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Agile1Tech\\Desktop\\Feb-2021-WS\\libraries\\chromedriver.exe");
+		
+	// create a object of the webdriver class and open up the browser 
+
+WebDriver driver = new ChromeDriver();
+		
+	// navigate to the url
+
+
+		
+		
+		
+		
+
+	}
+
+}
